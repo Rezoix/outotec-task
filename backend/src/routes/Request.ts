be4c4fa8f5f1554b0125ee6f700ba635193b7ee1
Saku Komulainen <saku.komulainen@aalto.fi>
2020-03-05 23:50:28 +0200

    Added backend for testdata

---
+++ backend/src/routes/Request.ts
@@ -0,0 +1,150 @@
+import * as Hapi from "@hapi/hapi";
+
+type Request = {
+  created: Date;
+  name: string;
+  type: string;
+  id: string;
+  description: string;
+  priority: string;
+  status: string;
+};
+
+const testData: Request[] = [
+  {
+    created: new Date(600000000000),
+    name: "Lorem ipsum",
+    type: "Audit",
+    id: "12312356345",
+    description: "Audit equipment",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(650000000000),
+    name: "Lorem ipsum",
+    type: "Audit",
+    id: "12312356346",
+    description: "Audit equipment",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(620500000000),
+    name: "Lorem ipsum",
+    type: "Audit",
+    id: "12312353345",
+    description: "Audit equipment",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(610000000000),
+    name: "Lorem ipsum",
+    type: "Audit",
+    id: "12312356745",
+    description: "Audit equipment",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(600500000000),
+    name: "Lorem ipsum",
+    type: "Audit",
+    id: "12312256345",
+    description: "Audit equipment",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(603000000000),
+    name: "Lorem ipsum",
+    type: "Audit",
+    id: "12212356345",
+    description: "Audit equipment",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(600005000000),
+    name: "Lorem ipsum",
+    type: "Audit",
+    id: "12312356344",
+    description: "Audit equipment",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(600000000000),
+    name: "Lorem ipsum",
+    type: "Maintenance",
+    id: "12312354344",
+    description: "Planned maintenance",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(650000000000),
+    name: "Lorem ipsum",
+    type: "Maintenance",
+    id: "12312354349",
+    description: "Planned maintenance",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(620500000000),
+    name: "Lorem ipsum",
+    type: "Maintenance",
+    id: "12312356340",
+    description: "Planned maintenance",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(610000000000),
+    name: "Lorem ipsum",
+    type: "Maintenance",
+    id: "12312350345",
+    description: "Planned maintenance",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(600500000000),
+    name: "Lorem ipsum",
+    type: "Maintenance",
+    id: "12312356305",
+    description: "Planned maintenance",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(603000000000),
+    name: "Lorem ipsum",
+    type: "Maintenance",
+    id: "12302356345",
+    description: "Planned maintenance",
+    priority: "High",
+    status: "open"
+  },
+  {
+    created: new Date(600005000000),
+    name: "Lorem ipsum",
+    type: "Maintenance",
+    id: "12312396340",
+    description: "Planned maintenance",
+    priority: "High",
+    status: "open"
+  }
+];
+
+const searchRequests: Hapi.ServerRoute = {
+  method: "GET",
+  path: "/requests",
+  handler: (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
+    return testData;
+  }
+};
+
+export { searchRequests };
---
+++ backend/src/server.ts
@@ -0,0 +1,24 @@
+const Hapi = require("@hapi/hapi");
+import { searchRequests } from "./routes/Request";
+
+const init = async () => {
+  const server = new Hapi.Server({
+    port: 3001,
+    host: "0.0.0.0",
+    routes: {
+      cors: true
+    }
+  });
+
+  server.route([searchRequests]);
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
